f32d316e8724d57e943de24d5475d347b96e58f3
JC <jacobconnell3@gmail.com>
2024-09-17 14:54:24 -0400

    Add; Functions to check if number is in sequence
    These functions will check if the user supplied number is in the
    fibonacci sequence before even starting to create the sequence itself

--- app.js
+++ app.js
@@ -14,3 +14,34 @@ document.getElementById('formFibonacci').addEventListener('submit', (formEvent) 
 // -----------------------------VV------------{Function for handling fibonacci generation}
 
 // -----------------------------VV------------{Function for checking if user supplied number falls into the fibonacci sequence}
+const checkForFibonacci = (inputNumber) => {
+    switch (inputNumber) {
+        case inputNumber < 0:
+            return false;
+            break;
+        case compareConditions(inputNumber) === true:
+            return true;
+            break;
+        case compareConditions(inputNumber) === false:
+            return false
+            break;
+    }
+}
+// -----------------------------VV------------{Function to compare user supplied number against fibonacci test conditions}
+const compareConditions = (x) => {
+    //-------------------VV-----------{Put number through two equations}
+    let testCondition1 = 5 * x ** 2 + 4;
+    let testCondition2 = 5 * x ** 2 + 4;
+    //-------------------VV----------{Check if at least one of the results is a perfect square}
+    return checkForPerfectSquare(testCondition1) || checkForPerfectSquare(testCondition2)
+}
+//----------------------------VV--------------{Function to test if a number is a perfect square}
+const checkForPerfectSquare = (num) => {
+    if (num < 0) {
+        return false;
+    }
+    // ------------------VV-----------{Gets the square root of the number}
+    const square = Math.sqrt(num);
+    // ------------------VV-----------{Checks if number is an integer, returns true if it is, returns false if it's not}
+    return Number.isInteger(square)
+};
