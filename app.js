4c59ae456d327178a6f08bfe098287f2dee0fd25
JC <jacobconnell3@gmail.com>
2024-09-17 15:14:04 -0400

    Fix; issue with incorrect operator and switch use
    Changed away from switch statement for easier more efficient function.
    Also fixed an issue in the compare conditions function

--- app.js
+++ app.js
@@ -3,38 +3,42 @@ document.getElementById('formFibonacci').addEventListener('submit', (formEvent) 
     //--------------------VV------------{prevent form submission from refreshing page}
     formEvent.preventDefault();
     //--------------------VV------------{Retrieve the value from the user input}
-    const userInput = document.getElementById("inputFibonacci").ariaValueMax;
+    const userInput = document.getElementById("inputFibonacci").value;
     console.log(userInput)
     //--------------------VV------------{Ensure that input is treated as a number}
     const inputNumber = userInput * 1
     console.log(inputNumber)
-    
+    console.log(checkForFibonacci(inputNumber))
+    if (checkForFibonacci(inputNumber)) {
+        console.log(`${inputNumber} is in the fibonacci sequence`)
+    } else {
+        console.log(`${inputNumber} is not in the fibonacci sequence`)
+    };
 
-})
+});
 // -----------------------------VV------------{Function for handling fibonacci generation}
 
 // -----------------------------VV------------{Function for checking if user supplied number falls into the fibonacci sequence}
 const checkForFibonacci = (inputNumber) => {
-    switch (inputNumber) {
-        case inputNumber < 0:
-            return false;
-            break;
-        case compareConditions(inputNumber) === true:
-            return true;
-            break;
-        case compareConditions(inputNumber) === false:
-            return false
-            break;
-    }
-}
+    // ------------ Check if number is less than zero
+    if (inputNumber < 0) {
+        return false;
+    // ------------ Check if compareConditions return true
+    } else if (compareConditions(inputNumber)){
+        return true;
+    // ----------- Only option left should be if compareConditions returns false
+    } else {
+        return false;
+    };
+};
 // -----------------------------VV------------{Function to compare user supplied number against fibonacci test conditions}
 const compareConditions = (x) => {
     //-------------------VV-----------{Put number through two equations}
     let testCondition1 = 5 * x ** 2 + 4;
-    let testCondition2 = 5 * x ** 2 + 4;
+    let testCondition2 = 5 * x ** 2 - 4;
     //-------------------VV----------{Check if at least one of the results is a perfect square}
-    return checkForPerfectSquare(testCondition1) || checkForPerfectSquare(testCondition2)
-}
+    return checkForPerfectSquare(testCondition1) || checkForPerfectSquare(testCondition2);
+};
 //----------------------------VV--------------{Function to test if a number is a perfect square}
 const checkForPerfectSquare = (num) => {
     if (num < 0) {
@@ -43,5 +47,5 @@ const checkForPerfectSquare = (num) => {
     // ------------------VV-----------{Gets the square root of the number}
     const square = Math.sqrt(num);
     // ------------------VV-----------{Checks if number is an integer, returns true if it is, returns false if it's not}
-    return Number.isInteger(square)
+    return Number.isInteger(square);
 };
